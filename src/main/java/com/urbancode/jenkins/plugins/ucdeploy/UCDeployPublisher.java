754fc33a39bcec443e15eafce1bc5db19a374520
Siddharth Bose <32789116+SidBose001@users.noreply.github.com>
2025-09-19 11:53:03 +0530

    Update UCDeployPublisher.java

--- src/main/java/com/urbancode/jenkins/plugins/ucdeploy/UCDeployPublisher.java
+++ src/main/java/com/urbancode/jenkins/plugins/ucdeploy/UCDeployPublisher.java
@@ -2,7 +2,7 @@
  * (c) Copyright IBM Corporation 2017.
  * This is licensed under the following license.
  * The Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
- * U.S. Government Users Restricted Rights:  Use, duplication or disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+ * U.S. Government Users Restricted Rights:  Use, duplication or disclosure restricted by GSA ADP Schedule Contract with IBM Cor p.
  */
 
 package com.urbancode.jenkins.plugins.ucdeploy;
